39d4cf35c247130d07b498f2817bedb5136e08b5
itlhd <xikan@armsom.org>
2026-02-06 14:32:23 +0800

    add: Initialize the Debian build script

--- mk-image.sh
+++ mk-image.sh
@@ -1,21 +1,32 @@
 #!/bin/bash -e
 
 TARGET_ROOTFS_DIR=./binary
-ROOTFSIMAGE=linaro-rootfs.img
-EXTRA_SIZE_MB=300
-IMAGE_SIZE_MB=$(( $(sudo du -sh -m ${TARGET_ROOTFS_DIR} | cut -f1) + ${EXTRA_SIZE_MB} ))
 
+if [ $RK_ROOTFS_IMAGE ]; then
+	ROOTFSIMAGE=$RK_ROOTFS_IMAGE
+else
+	ROOTFSIMAGE=linaro-$SOC-$TARGET-rootfs.img
+fi
 
 echo Making rootfs!
 
 if [ -e ${ROOTFSIMAGE} ]; then
-	sudo rm ${ROOTFSIMAGE}
+	rm ${ROOTFSIMAGE}
 fi
 
-for script in ./post-build.sh ../device/rockchip/common/post-build.sh; do
-	[ -x $script ] || continue
-	sudo $script "$(realpath "$TARGET_ROOTFS_DIR")"
-done
+# for script in ./post-build.sh ../device/rockchip/common/post-build.sh; do
+# 	[ -x $script ] || continue
+# 	sudo $script "$(realpath "$TARGET_ROOTFS_DIR")"
+# done
+
+sudo ./add-build-info.sh ${TARGET_ROOTFS_DIR}
+
+# Apparent size + maxium alignment(file_count * block_size) + maxium journal size
+IMAGE_SIZE_MB=$(( $(sudo du --apparent-size -sm ${TARGET_ROOTFS_DIR} | cut -f1) + \
+	$(sudo find ${TARGET_ROOTFS_DIR} | wc -l) * 4 / 1024 + 64 ))
+
+# Extra 10%
+IMAGE_SIZE_MB=$(( $IMAGE_SIZE_MB * 110 / 100 ))
 
 sudo mkfs.ext4 -d ${TARGET_ROOTFS_DIR} ${ROOTFSIMAGE} ${IMAGE_SIZE_MB}M
 
